324db388431a6dd95fcb490dfbf0e313e6e61e20
Stupendos <topakovdenis671@gmail.com>
2024-10-31 19:08:32 +0700

    ЫЫЫ

---
+++ src/main/java/org/example/calculatorhw/CalculatorHwApplication.java
@@ -0,0 +1,13 @@
+package org.example.calculatorhw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorHwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorHwApplication.class, args);
+    }
+
+}
